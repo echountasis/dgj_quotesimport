2c9d41341b2ab291eb4c0e586367b4b8988c441f
Emmanouil Chountasis <echountasis@outlook.com>
2021-03-27 19:15:49 +0100

    added py file

---
+++ migrate.py
@@ -0,0 +1,26 @@
+import csv
+import requests
+
+
+URL = "https://10au8vx47a.execute-api.us-east-2.amazonaws.com/prod/quote"
+
+with open('XXXXXX.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            line_count += 1
+
+            PARAMS = {
+                "dayOfYear": int(row[1]),
+                "year": int(row[4]),
+                "quote": row[2],
+                "author": row[3]
+            }
+            r = requests.post(url=URL, json=PARAMS)
+            print(line_count, r)
+
+    print(f'Processed {line_count} lines.')
