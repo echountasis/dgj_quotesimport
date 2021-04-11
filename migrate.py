d176baaeb0a9070debbaaf3c143daf0cd212e67f
Emmanouil Chountasis <echountasis@outlook.com>
2021-04-11 14:46:34 +0200

    added quotes till 6th november

--- migrate.py
+++ migrate.py
@@ -4,7 +4,7 @@ import requests
 
 URL = "https://10au8vx47a.execute-api.us-east-2.amazonaws.com/prod/quote"
 
-with open('XXXXXX.csv') as csv_file:
+with open('quotes3.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
